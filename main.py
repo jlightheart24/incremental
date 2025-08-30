0262fac839f0583680653711ccfb5e958089b14a
Jonny Lightheart <jonnylightheart@gmail.com>
2025-08-30 10:25:11 -0600

    testing git hub repsitory and chatgpt connection

---
+++ main.py
@@ -0,0 +1,9 @@
+def testObject():
+    """testing repository connections and chatgpt connection
+    I am trying to make an incremental game and I want to make a object the can update over a certain amount of tics
+    right now I am testing to see how chatgpt interacts with github to see if it can help me code this object"""
+
+def main():
+    pass
+
+main()
