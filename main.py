1d1f984d7f5bebe211e5b9e88a81a1ef5737f829
jlightheart <jonnylightheart@gmail.com>
2025-09-04 17:48:02 -0600

    Added functionality to main to allow for testing of the item object code

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-def testObject():
-    """testing repository connections and chatgpt connection
-    I am trying to make an incremental game and I want to make a object the can update over a certain amount of tics
-    right now I am testing to see how chatgpt interacts with github to see if it can help me code this object"""
+import math
+import item
+
+def createItem(name, base_price, base_stat):
+    new_item = item.Item(name, base_price, base_stat)
+    print("Created Item")
+    print(str(new_item))  # Calls new_item.__str__()
+    return new_item
 
 def main():
-    pass
+    money = 10
+    print(f"You have {money} gold.")    
+    sword = createItem("sword", 1, 1)
+    for i in range(10):
+        purchase = input("Do you want to purchase a sword? Y/N: ")
+        if purchase.lower() == 'y':
+            if money < sword.price:
+                print("You don't have enough gold!")
+                continue
+            sword.purchase()
+            money -= sword.price
+            money = math.ceil(money)
+            print(f"You have {money} gold left.")
+        print(sword)
 
 main()
